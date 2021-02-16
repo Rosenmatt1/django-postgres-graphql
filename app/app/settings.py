b4e3748bcb422f486abff29780e72be6dbafa8b7
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-02-15 19:54:09 -0700

    Updated graphene in app.urls

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,17 +31,23 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'graphene_django'
-    'pokemon'
+    'graphene_django',
+    'pokemon',
 ]
 
-GRAPHENE = {'app.schema.schema'}
+GRAPHENE = {
+    'SCHEMA': 'app.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
