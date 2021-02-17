aa0aed8cbb1614b4e0f560881dbb07cefa90c9ba
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-02-16 23:20:07 -0700

    Added User query and mutation

--- app/app/schema.py
+++ app/app/schema.py
@@ -1,10 +1,11 @@
 import graphene
 import pokemon.schema
+import users.schema
 
-class Query(pokemon.schema.Query, graphene.ObjectType):
+class Query(users.schema.Query, pokemon.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(pokemon.schema.Mutation):
+class Mutation(users.schema.Mutation, pokemon.schema.Mutation):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
---
+++ app/users/schema.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import get_user_model
+import graphene
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+        # only_fields = ('id', 'email', 'password', 'username')   This would make it so the default fields such as SuperUser are not included
+
+class Query(graphene.ObjectType):
+    user = graphene.Field(UserType, id=graphene.Int(required=True))
+    me = graphene.Field(UserType)
+
+    def resolve_user(self, info, id):
+        return get_user_model().objects.get(id=id)
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not Logged in!')
+        
+        return user
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+    
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+    
+    def mutate(self, info, username, password, email):
+        user = get_user_model()(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
