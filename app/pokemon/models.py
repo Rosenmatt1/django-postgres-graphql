63d4bf49635f81afce32f950d4ee2d88a7cd90ad
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-02-21 21:31:58 -0700

    Added posted_by migration

--- app/pokemon/models.py
+++ app/pokemon/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
 # Create your models here.
 class Pokemon(models.Model):
     #id is added automatically
@@ -7,3 +9,6 @@ class Pokemon(models.Model):
     abilities = models.TextField(blank=True) #allows to be left blank. Database can hold a null value
     power_level = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    posted_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)   #ForeignKey allows for many to one relationship.  So a user can create many pokemon
+    #null=True allows for migrations to be compatible with Django version 2
+    #on_delete=models.CASCADE if  User is deleted, the pokemon it created will also be deleted
