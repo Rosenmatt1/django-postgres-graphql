ab5ea7f84f03283e18c5f5f5e12598139329a25a
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-02-27 20:10:42 -0700

    Added tailwind to front end

--- pokemon-front-end/src/App.js
+++ pokemon-front-end/src/App.js
@@ -4,6 +4,8 @@ import Counter from './components/Counter.js'
 import Reset from './components/Reset.js'
 
 
+//Use Tailwind for styling!!!!!!
+
 function App() {
   return (
     <div className="App">
