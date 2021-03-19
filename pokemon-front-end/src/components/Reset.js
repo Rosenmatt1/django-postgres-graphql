18c11d3fd1c1c125e0ec53a12c85c8a37358341b
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-03-18 20:08:26 -0600

    Added ResetDeck Mutation

--- pokemon-front-end/src/components/Reset.js
+++ pokemon-front-end/src/components/Reset.js
@@ -1,23 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css';
 
+import Loader from './Shared/Loader.js'
+import Error from './Shared/Error.js'
+
+import { Query, Mutation } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
 function Reset() {
   // const [count, setCount] = useState(52);
- 
+
 
   // Similar to componentDidMount and componentDidUpdate:
-//   useEffect(() => {
-    
-//   });
+  //   useEffect(() => {
+
+  //   });
 
   return (
-    <div className="resetContainer">
-        <div className="reset">
-            Reset
-        </div>
-    </div>
+    <Mutation
+      mutation={RESET_MUTATION}
+      onCompleted={data => {
+        console.log("it worked!!!!")
+      }}
+    // update={handleUpdateCache}
+    // refetchQueries={() => [{ query: GET_TRACKS_QUERY }]}  //could also use graphQL subscriptions
+    >
+      {(resetDeck, { loading, error }) => {
+        if (error) return <Error error={error} />
+        return (
+          <div className="resetContainer" onClick={() => resetDeck()}>
+            <div className="reset">
+              Reset
+            </div>
+          </div>
+        )
+      }}
+    </Mutation>
   );
 }
+
+const RESET_MUTATION = gql`
+  mutation {
+    resetDeck {
+      card {
+        name
+        suit
+        color
+        active 
+        used
+      }
+    }
+  }
+`
 
 
 export default Reset;
