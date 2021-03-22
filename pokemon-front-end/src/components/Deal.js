17baf096434e71ba308763476044f69f8d7992d0
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-03-21 18:41:42 -0600

    Successfully was able to link Deal mutation with a random id

--- pokemon-front-end/src/components/Deal.js
+++ pokemon-front-end/src/components/Deal.js
@@ -11,14 +11,39 @@ import { Query, Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
 
 function Deal(data) {
-  console.log("DATA!!", data)
+  // console.log("DATA!!", data)
   let cards = data.data.cards
+
+  // let arrayOfIds = []
+  let randomCard = null
+  let findIndex = null
+  let card1 = null
+  let card1ID = null
+
+  // card1 = cards[randomCard]
+  // console.log("card1", card1)
+
+  const generateRandomCards = (dealHand) => {
+    console.log("cards", cards)
+    // console.log("Length", cards.length)
+
+    randomCard = Math.floor(Math.random() * (cards.length))
+    console.log("randomCard", randomCard)
+
+    // findIndex = cards.indexOf(randomCard)
+    // console.log("findIndex which should be one less", findIndex)
+
+    card1 = cards[randomCard]
+    card1ID = parseInt(card1.id)
+    console.log("card1 id!!!!!", typeof(card1ID))
+    dealHand({ variables: { card1Id: card1ID  } })
+  }
 
   return (
     <div className="deal">
       <Mutation
         mutation={DEAL_MUTATION}
-        variables={{ card1Id: 1 }}
+        // variables={{ card1Id: card1ID }}
         onCompleted={data => {
           // randomCards()
           console.log("it worked!!!!")
@@ -29,7 +54,7 @@ function Deal(data) {
         {(dealHand, { loading, error }) => {
           if (error) return <Error error={error} />
           return (
-            <div onClick={()=> dealHand()}>
+            <div onClick={() => generateRandomCards(dealHand)}>
               Deal
             </div>
           )
@@ -38,6 +63,8 @@ function Deal(data) {
     </div>
   );
 }
+
+// generateRandomCards(dealHand, cards)
 
 const DEAL_MUTATION = gql`
   mutation($card1Id: Int!) {
