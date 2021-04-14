305a1f2e11223617de46a1937552981045ead75b
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-04-14 00:05:23 -0600

    Created a data file

---
+++ pokemon-front-end/src/data.js
@@ -0,0 +1,96 @@
+let data = [
+  {
+    id: 1,
+    name: "A",
+    suit: "heart",
+    color: "red",
+    used: "false",
+  },
+  {
+    id: 2,
+    name: "K",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 3,
+    name: "Q",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 4,
+    name: "J",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 5,
+    name: "10",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 6,
+    name: "9",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 7,
+    name: "8",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 8,
+    name: "7",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 9,
+    name: "6",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 10,
+    name: "5",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 11,
+    name: "4",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 12,
+    name: "3",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  },
+  {
+    id: 13,
+    name: "2",
+    suit: "heart",
+    color: "red",
+    used: "false"
+  }
+]
+
+
+export default data;
